a806ed7f8f59765fc934e740937888c42c4332de
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-26 19:02:38 +0530

    Create register.php

---
+++ Day24_Assignment_huzaifa/register.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$conn = new mysqli("localhost", "root", "", "user_auth");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // hash password
+
+    $stmt = $conn->prepare("INSERT INTO users (name, email, password) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $name, $email, $password);
+
+    if ($stmt->execute()) {
+        echo "Registration successful. <a href='login.php'>Login Now</a>";
+    } else {
+        echo "Error: " . $conn->error;
+    }
+    $stmt->close();
+}
+?>
+
+<form method="post" action="">
+    <h2>Register</h2>
+    Name: <input type="text" name="name" required><br><br>
+    Email: <input type="email" name="email" required><br><br>
+    Password: <input type="password" name="password" required><br><br>
+    <input type="submit" value="Register">
+</form>
